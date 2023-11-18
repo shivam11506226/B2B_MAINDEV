996694f47f7e3d88a40e6456970ac13eb1dc59c9
Qamar Ali <qamarali205@gmail.com>
2023-11-18 18:47:47 +0530

    administration

--- src/Pages/Account/FlightTicket.jsx
+++ src/Pages/Account/FlightTicket.jsx
@@ -2,19 +2,41 @@ import React, { useState } from "react";
 import FlightChangeReq from "./FlightChangeReq";
 import HotelChangeReq from "./HotelChangeReq";
 import "./FlightTicket.css";
+import BusChangeReq from "./BusChangeReq";
+import HolidayChangeReq from "./HolidayChangeReq";
+import { GiTreasureMap } from "react-icons/gi";
 
 const FlightTicket = () => {
   const [showFlightChangeReq, setShowFlightChangeReq] = useState(true);
   const [showHotelChangeReq, setShowHotelChangeReq] = useState(false);
+  const [showBusChangeReq, setShowBusChangeReq] = useState(false);
+  const [showHolidayChangeReq, setShowHolidayChangeReq]=useState(false);
 
   const handleFlightButtonClick = () => {
     setShowFlightChangeReq(true);
     setShowHotelChangeReq(false);
+    setShowBusChangeReq(false);
+    setShowHolidayChangeReq(false);
   };
 
   const handleHotelButtonClick = () => {
     setShowFlightChangeReq(false);
     setShowHotelChangeReq(true);
+    setShowBusChangeReq(false);
+    setShowHolidayChangeReq(false);
+  };
+
+  const handleBusButtonClick = () => {
+    setShowFlightChangeReq(false);
+    setShowHotelChangeReq(false);
+    setShowBusChangeReq(true);
+    setShowHolidayChangeReq(false);
+  };
+  const handleHolidayButtonClick = () => {
+    setShowFlightChangeReq(false);
+    setShowHotelChangeReq(false);
+    setShowBusChangeReq(false);
+    setShowHolidayChangeReq(true);
   };
 
   return (
@@ -22,10 +44,13 @@ const FlightTicket = () => {
       <div className="buttonBox">
         <button onClick={handleFlightButtonClick}>Flight</button>
         <button onClick={handleHotelButtonClick}>Hotel</button>
-        <button onClick={handleHotelButtonClick}>Bus</button>
+        <button onClick={handleBusButtonClick}>Bus</button>
+        <button onClick={handleHolidayButtonClick}>Holiday</button>
       </div>
       {showFlightChangeReq && <FlightChangeReq />}
       {showHotelChangeReq && <HotelChangeReq />}
+      {showBusChangeReq && <BusChangeReq />}
+      {showHolidayChangeReq && <HolidayChangeReq />}
     </div>
   );
 };
